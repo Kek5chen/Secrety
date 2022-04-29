2ec0849a29822e5058200fc728e0e4e09fed3558
Kekschen <52585984+Kek5chen@users.noreply.github.com>
2022-04-29 16:33:01 +0200

    style: added greeting message

--- CrackMes/main.cpp
+++ CrackMes/main.cpp
@@ -9,6 +9,9 @@ int main() {
 
 	antiDebug();
 
+	int inputSerialKeyChars[] = { -20, 8, 1, -3, 15, 1, -68, 5, 10, 12, 17, 16, -68, 21, 11, 17, 14, -68, 15, 1, 14, 5, -3, 8, 7, 1, 21, -42 };
+	out::printOut(inputSerialKeyChars, _countof(inputSerialKeyChars), false);
+
 	while (true) {
 	}
 	return 0;
